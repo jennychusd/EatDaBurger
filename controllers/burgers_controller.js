99947d3c44a7d0fa27c21c0c2eaea28169627b19
Jenny Chu <jennychu.sd@gmail.com>
2017-04-26 21:21:06 -0700

    added server.js and basic html page

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,6 +5,12 @@ var router = express.Router();
 // Import the model to use its database functions.
 var cat = require("../models/burger.js");
 
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+
+    res.render("index");
+
+});
 
 // Export routes for server.js to use.
 module.exports = router;
