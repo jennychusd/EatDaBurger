fc480633ffaf7bcd11ea6dbafa76386099ccbcc1
Jenny Chu <jennychu.sd@gmail.com>
2017-04-26 21:26:12 -0700

    edited schema and seeds sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,13 @@
 ### Schema
 
-CREATE DATABASE burgers_db;
-USE burgers_db;
+CREATE DATABASE `burgers_db`;
+USE `burgers_db`;
 
-CREATE TABLE burgers
+CREATE TABLE `burgers`
 (
-	id int NOT NULL AUTO_INCREMENT,
-	burger varchar(255) NOT NULL,
-	devoured BOOLEAN DEFAULT false,
-    date TIMESTAMP,
-	PRIMARY KEY (id)
+	`id` int NOT NULL AUTO_INCREMENT,
+	`burger` varchar(144) NOT NULL,
+	`devoured` BOOLEAN DEFAULT false,
+    `date` DATETIME DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`id`)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,4 @@
-INSERT INTO burgers (burger, devoured, date)
-VALUES ('Grilled Chicken Burger', true, '2017-04-24 08:10:00.000000'),
-('Double Cheeseburger', true, '2017-03-23 16:45:00.000000');
-
-INSERT INTO burgers (burger, devoured)
-VALUES ('Veggie Burger', false);
+INSERT INTO `burgers` (`burger`, `devoured`)
+VALUES ('Grilled Chicken Burger', true),
+('Double Cheeseburger', true),
+('Veggie Burger', false);
