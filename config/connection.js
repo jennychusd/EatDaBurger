d899a09f97d4f12fd27e2676465a857f16e0d118
Jenny Chu <jennychu.sd@gmail.com>
2017-04-29 23:11:57 -0700

    revert back to using localhost

--- config/connection.js
+++ config/connection.js
@@ -3,10 +3,10 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   port: 3306,
-  host: "us-cdbr-iron-east-03.cleardb.net",
-  user: "bd6807a60ee3f1",
-  password: "8a31bb7d",
-  database: "heroku_7aab7fd98f89c29"
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "burgers_db"
 });
 
 // Make connection.
